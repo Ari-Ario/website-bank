0efd86f9f4a2e69f9d2929a5df08bbd232bad07f
Ari-Ario <xusrew.z@gmail.com>
2023-12-21 11:36:26 +0100

    the image 2 will be replaced if screen is less than 600px, in javascript solved

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ function closePopup() {
     popup.style.display = "none";
 }
 
+/*   window.onload = () => { 
+    replaceImage()
+    window.addEventListener('resize', replaceImage);
+  } */
+
+
+
+function windowSize(){
+    if ( screen.width < 600) {     
+        console.log("1")
+        const secondImage= document.getElementById("bank-img-2");
+        let img = document.createElement("img");
+        img.src= "photos/swiss-banking.jpg";
+        img.id= "replaced-img"
+        img.style.marginBottom = "40px"
+        const newElement= document.getElementById("img-place");
+        newElement.append(img);
+        document.getElementById("replaced-img").style.maxWidth = "100%";
+        secondImage.style.display = "none";
+    }
+    else {
+        console.log("2")
+    }
+}
+
